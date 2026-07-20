adb482ea85cc8db54b10645a013e94c2208c2704
MaxwellGuoDUT <maxwell.guo13@gmail.com>
2026-07-21 00:00:06 +0800

    Day 3-2: Implement life progression logic

--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
 import { Player } from "./player.js";
+import { EventManager } from "./event.js";
 
 export class Game {
     constructor() {
         this.player = null;
+        this.eventManager = new EventManager();
     }
 
     startNewGame() {
@@ -10,5 +12,17 @@ export class Game {
 
         console.log("新的斗罗人生开始！");
         console.log(this.player);
+
+        return this.getCurrentEvent();
+    }
+
+    nextYear() {
+        this.player.age += 1;
+
+        return this.getCurrentEvent();
+    }
+
+    getCurrentEvent() {
+        return this.eventManager.getEventByAge(this.player.age);
     }
 }
