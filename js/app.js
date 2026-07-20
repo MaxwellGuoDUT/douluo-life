5b66ebb61f5d2fa3d6e358ef20c24e8224fe3def
MaxwellGuoDUT <maxwell.guo13@gmail.com>
2026-07-21 00:04:32 +0800

    Day 3-4: Connect life cycle UI

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 import { Game } from "./game.js";
+import { UI } from "./ui.js";
 
 const game = new Game();
+const ui = new UI();
 
 const newGameBtn = document.getElementById("newGameBtn");
+const nextYearBtn = document.getElementById("nextYearBtn");
 
 newGameBtn.addEventListener("click", () => {
-    game.startNewGame();
+    const event = game.startNewGame();
+
+    ui.showGameArea();
+    ui.renderPlayer(game.player);
+    ui.renderEvent(event);
+});
+
+nextYearBtn.addEventListener("click", () => {
+    const event = game.nextYear();
+
+    ui.renderPlayer(game.player);
+    ui.renderEvent(event);
 });
